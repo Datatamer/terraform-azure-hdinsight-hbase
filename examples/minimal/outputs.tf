f3393b18948a057808aed5f5d372aa985bb3c81c
Ben Schwartzman <ben.schwartzman@tamr.com>
2020-11-18 18:33:39 -0500

    Add missing outputs

--- examples/minimal/outputs.tf
+++ examples/minimal/outputs.tf
@@ -12,3 +12,13 @@ output "hbase_cluster_id" {
   value       = module.hdinsight.hbase_cluster_id
   description = "The ID of the HDInsight HBase cluster"
 }
+
+output "storage_account_primary_access_key" {
+  value       = module.hdinsight.storage_account_primary_access_key
+  description = "Primary access key of the storage account"
+}
+
+output "storage_account_name" {
+  value       = module.hdinsight.storage_account_name
+  description = "Name of the storage account"
+}
